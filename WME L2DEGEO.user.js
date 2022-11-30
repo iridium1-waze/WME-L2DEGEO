347a7007014c7c93100c6eed659296bedffa408c
Iridium1 <70545967+iridium1-waze@users.noreply.github.com>
2022-11-30 21:44:46 +0100

    Update WME L2DEGEO.user.js

--- WME L2DEGEO.user.js
+++ WME L2DEGEO.user.js
@@ -2,7 +2,7 @@
 // @name    WME Link to German States Geo Portals
 // @description This script create buttons to open Geo portals of German states, using the WME paramenters where supported.
 // @namespace  https://github.com/iridium1-waze/WME-L2DEGEO/blob/main/WME%20L2DEGEO.user.js
-// @version   2022.11.20.01
+// @version   2022.11.30.01
 // @include   https://*.waze.com/editor*
 // @include   https://*.waze.com/*/editor*
 // @grant	none
@@ -12,7 +12,7 @@
 // Mini howto:
 // 1) install this script as GitHub script
 // 2) Click on any of the links includes to open the state GEO portal, PL Data will be handed over where supported.
-var l2degeo_version = "2022.11.20.01";
+var l2degeo_version = "2022.11.30.01";
 // by Iridium1 (contact either PM or iridium1.waze@gmail.com)
 // 2021.01.17.01: Initial release
 // 2021.04.12.01: Changed URL for Brandenburg Viewer
@@ -24,6 +24,7 @@ var l2degeo_version = "2022.11.20.01";
 // 2022.10.02.01: Fixed Link for GeoPortal Hamburg (Thanks to s.jay.m!)
 // 2022.10.02.02: Changed Zoomlevel for GeoPortal Hamburg
 // 2022.11.20.01: Removed handing over of coordinates to Portal Thüringen as no longer supported (Thanks for the hint FasterinoSpeederino!)
+// 2022.11.20.01: Re-Added Thüringen Portal - Hint from LostEmber (Thanks for your contribution!) of how to (still) hand over coordinates.
 
 /* eslint-env jquery */ //we are working with jQuery
 //indicate used variables to be assigned
@@ -313,13 +314,38 @@ function popAtlas() {
 }
 });
 
-var thu_btn = $('<button style="width: 285px;height: 24px; font-size:85%;color: DarkSlateGrey;border-radius: 5px;border: 0.5px solid lightgrey; background: white">Geoportal Thüringen</button>');
+
+var thu_btn = $('<button style="width: 285px;height: 24px; font-size:85%;color: Green;border-radius: 5px;border: 0.5px solid lightgrey; background: white">Geoportal Thüringen</button>');
 thu_btn.click(function(){
 
+  var href = $('.WazeControlPermalink a').attr('href');
+
+  var lon = parseFloat(getQueryString(href, 'lon'));
+  var lat = parseFloat(getQueryString(href, 'lat'));
+  var zoom = parseInt(getQueryString(href, 'zoom')) + CorrectZoom(href);
+
   var mapsUrl = 'https://thueringenviewer.thueringen.de/thviewer/';
+  zoom = zoom-9;ac
+
+  // Using Proj4js to transform coordinates. See http://proj4js.org/
+  var script = document.createElement("script"); // dynamic load the library from https://cdnjs.com/libraries/proj4js
+  script.type = 'text/javascript';
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.4.4/proj4.js';
+  document.getElementsByTagName('head')[0].appendChild(script); // Add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+  script.onload = popAtlas; //wait till the script is downloaded & executed
+  function popAtlas() {
+   //just a wrapper for onload
+     if (proj4) {
+       var firstProj ='';
+         firstProj = "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
+           var utm = proj4(firstProj,[lon,lat]);
+  var mapsUrl = 'https://thueringenviewer.thueringen.de/thviewer/?center=' + utm[0] + ',' + utm [1] +'&zoomlevel=' +zoom;
   window.open(mapsUrl,'_blank');
+   }
+  }
 
 });
+
 
 var deu_btn = $('<button style="width: 285px;height: 24px; font-size:85%;color: Green;border-radius: 5px;border: 0.5px solid lightgrey; background: white">WebAtlas Deutschland</button>');
 deu_btn.click(function(){
